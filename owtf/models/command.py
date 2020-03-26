70c1b4b4efd3d2386e8b5ffc199a0eb9f65c4ee8
sharmamohit123 <mohitdonsharma9@gmail.com>
2020-03-27 03:41:34 +0530

    fix travis 2

--- owtf/models/command.py
+++ owtf/models/command.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 from owtf.db.model_base import Model
 from owtf.db.session import flush_transaction
-from . import plugin
 
 
 class Command(Model):
@@ -18,7 +17,7 @@ class Command(Model):
     end_time = Column(DateTime)
     success = Column(Boolean, default=False)
     target_id = Column(Integer, ForeignKey("targets.id"))
-    plugin_key = Column(String, ForeignKey("plugins.key"))
+    plugin_key = Column(String)
     modified_command = Column(String)
     original_command = Column(String, primary_key=True)
 
